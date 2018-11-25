6cb81c3bb491d50efbfac9c7dc4941eae0bdd2af
Rafael Cruz <ralph1786@gmail.com>
2018-11-25 23:08:05 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,6 +4,11 @@ def dictionary
     "to" => "2",
     "two" => "2",
     "too" => "2",
-    
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end
