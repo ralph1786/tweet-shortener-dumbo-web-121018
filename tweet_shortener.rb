de589c78f67be5026a15b77cee0fb1cefb148533
Rafael Cruz <ralph1786@gmail.com>
2018-11-25 23:28:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  word_substituter(tweet) > 140 ? w
+  word_substituter(tweet).length > 140 ? word_substituter
   
 end
 
